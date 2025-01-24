d231564b7724defedfbfa2ca47088a7d7797d49b
jacob-myers <jakemyers190@gmail.com>
2025-01-24 15:27:39 -0500

    Restyling and adding some images for use. Home screen is being made to look like a business card.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React from "react";
 import './App.css';
 import './styles.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 // Imported components HAVE to start with a capital or they will be ignored, no error
 import JNavbar from './components/Navbar';
 import {
@@ -10,15 +11,52 @@ import {
   Routes,
   Navigate
 } from "react-router-dom";
+import { Container, Row, Col } from "react-bootstrap";
+import azamiImg from "../src/Assets/azami.png";
+import selfImg from "../src/Assets/self.png"
+import HomeCard from "./components/Home/HomeCard";
 
 // React rotating image.
 // <img src={logo} className="App-logo" alt="logo" />
 
 function App() {
+  // For the actual document itself.
+  document.body.style = "background: black";
   return (
     <div className="App">
       <title>Jake Myers</title>
       <JNavbar />
+
+      <Container fluid className="body">
+        <Row style={{ display: 'flex', justifyContent: "center", padding: "10px" }}>
+          <Col
+            className="content-col"
+            md={6}
+            style={{
+              justifyContent: "right",
+              paddingTop: "30px",
+              paddingBottom: "50px",
+              width: "500px"
+            }}
+          >
+            <HomeCard/>
+          </Col>
+          
+          <Col
+            className="content-col"
+            md={6}
+            style={{
+              justifyContent: "left",
+              paddingTop: "30px",
+              paddingBottom: "50px",
+              width: "700px"
+            }}
+          >
+            <img src={selfImg} alt="art" className="img-self" />
+          </Col>
+        </Row>
+
+      </Container>
 
       <div>
         <a
---
+++ src/components/Home/HomeCard.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+
+function HomeCard() {
+    return (
+        <blockquote className="blockquote home" style={{ margin:"0" }}>
+            <p style={{ fontSize:"2.1em" }} className="text-classic">
+                <span className="text-bold">Jake Myers</span>
+                <br/>
+                Software Engineer | Artist
+            </p>
+        </blockquote>
+  );
+}
+
+export default HomeCard;
