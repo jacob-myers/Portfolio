884ed2426d7edf78e820e47106072b0ae12fe46a
jacob-myers <jakemyers190@gmail.com>
2025-01-28 04:25:53 -0500

    Adding dragon image and styling/arranging it to fit next to my card on About page.

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,16 +1,42 @@
 import React from "react";
-import { Container} from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 
 import Skillstack from "./Skillstack";
 import Languagestack from "./Languagestack";
 import Toolstack from "./Toolstack";
 import AboutCard from "./AboutCard";
 
+import dragonImg from "../../Assets/DragonBW.png"
+
 function About() {
     return (
         <Container fluid className="page-body">
             <Container>
-                <AboutCard />
+                <Row style={{ display:'flex', justifyContent:"center" }}>
+                    <Col
+                        className="content-col"
+                        style={{
+                            justifyContent: "center",
+                            paddingTop: "30px",
+                            paddingBottom: "30px",
+                            minWidth: "500px"
+                        }}
+                    >
+                        <AboutCard />
+                    </Col>
+                    <Col
+                        className="content-col"
+                        style={{
+                            justifyContent: "center",
+                            paddingTop: "30px",
+                            paddingBottom: "30px",
+                            minWidth: "500px",
+                        }}
+                    >
+                        <img src={dragonImg} alt="dragon" className="img-dragon" />
+                    </Col>
+                </Row>
+                
                 <Container style={{height:"40px"}} />
                 <h1 className="project-header text-classic">Languages</h1>
                 <Languagestack />
--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -1,18 +1,21 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 
 function AboutCard() {
     return (
-        <blockquote className="blockquote home" style={{ margin:"0", textAlign:"left"}}>
-            <h1 className="project-header text-bold">Who I Am</h1>
-            <p style={{ fontSize:"1.1em" }} className="text-classic">
-                Hi, I am <span className="text-bold">Jacob Myers </span> 
-                from <span className="text-bold">New Hampshire, USA.</span>
-                <br/> <br/>
-                Professionally I am a software engineer and am always interested in new opportunities for work.
-                I hold a bachelors of science in <span className="text-bold">Computer Science and Innovation </span>from Champlain College.
-                I am also an experienced digital and sketch artist. All artwork used on this site is mine.
-            </p>
-        </blockquote>
+        <Container className="about-quote">
+            <blockquote className="blockquote" style={{ margin:"0", textAlign:"left"}}>
+                <h1 className="project-header text-bold">Who I Am</h1>
+                <p style={{ fontSize:"1.1em" }} className="text-classic">
+                    Hi, I am <span className="text-bold">Jacob Myers </span> 
+                    from <span className="text-bold">New Hampshire, USA.</span>
+                    <br/> <br/>
+                    Professionally I am a software engineer and am always interested in new opportunities for work.
+                    I hold a bachelors of science in <span className="text-bold">Computer Science and Innovation </span>from Champlain College.
+                    I am also an experienced digital and sketch artist. All artwork used on this site is mine.
+                </p>
+            </blockquote>
+        </Container>
   );
 }
 
