9872bb6cf6fcea75f1445a783b5b6d4cdb013854
jacob-myers <jakemyers190@gmail.com>
2025-01-28 01:14:51 -0500

    Adding a bio above skills on About page.

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,20 +4,21 @@ import { Container} from "react-bootstrap";
 import Skillstack from "./Skillstack";
 import Languagestack from "./Languagestack";
 import Toolstack from "./Toolstack";
+import AboutCard from "./AboutCard";
 
 function About() {
     return (
         <Container fluid className="page-body">
             <Container>
+                <AboutCard />
+                <Container style={{height:"40px"}} />
                 <h1 className="project-header text-classic">Languages</h1>
                 <Languagestack />
                 <h1 className="project-header text-classic">Skills</h1>
                 <Skillstack />
                 <h1 className="project-header text-classic">Tools</h1>
                 <Toolstack />
-                <Container style={{height:"80px"}}>
-
-                </Container>
+                <Container style={{height:"80px"}} />
             </Container>
         </Container>
     );
