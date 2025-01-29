0838571dcd54b5ba9651ebfce6920f6a24a0058c
jacob-myers <jakemyers190@gmail.com>
2025-01-29 02:40:03 -0500

    react-pdf will not work under any circumstance consistantly. Completely random. Removing it and using iframe with download button.

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,21 +1,14 @@
 import React from "react";
 import { Container, Col, Button } from "react-bootstrap";
-import { Document, Page, pdfjs } from "react-pdf";
 
 import { IoMdDownload } from "react-icons/io";
 
 import pdf from "../../Assets/Resume-Jacob_Myers_4.pdf";
 
-import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-    'pdfjs-dist/build/pdf.worker.min.mjs',
-    import.meta.url,
-  ).toString();
-
 function Resume() {
     return (
-        <Container fluid className="page-body">
-            <Col>
+        <Container fluid className="page-body" style={{ height:"100vh" }}>
+            <Col style={{height:"85%"}}>
                 <Button
                     className="project-buttons"
                     variant="primary"
@@ -27,26 +20,9 @@ function Resume() {
                     &nbsp;Download Resume
                 </Button>
                 
-                {/* 
-                <Container style={{height:"84vh"}}>
-                    <iframe title="hi" src={pdf} width="100%" height="100%" />
+                <Container style={{height:"100%"}}>
+                    <iframe title="resume" src={pdf} width="100%" height="100%" />
                 </Container>
-                */}
-
-                <Document file={pdf} className="d-flex justify-content-center">
-                    <Page pageNumber={1} scale={1.5} />
-                </Document> 
-
-                <Button
-                    className="project-buttons"
-                    variant="primary"
-                    href={pdf}
-                    target="_blank"
-                    style={{ maxWidth: "200px", marginTop:"20px" }}
-                >
-                    <IoMdDownload size={18} />
-                    &nbsp;Download Resume
-                </Button>
             </Col>
         </Container>
     );
