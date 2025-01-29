25d8e63d6af014d58bd439d29c466c1dac8ac573
jacob-myers <jakemyers190@gmail.com>
2025-01-29 01:33:36 -0500

    Modile doesn't embed with the iframe, so switching back to Document system.

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,24 +1,21 @@
 import React from "react";
-import { Container, Col} from "react-bootstrap";
-//import { pdfjs } from "react-pdf";
+import { Container, Col, Button } from "react-bootstrap";
+import { Document, Page, pdfjs } from "react-pdf";
 
-//import { IoMdDownload } from "react-icons/io";
+import { IoMdDownload } from "react-icons/io";
 
 import pdf from "../../Assets/Resume-Jacob_Myers_4.pdf";
 
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-/*
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
     'pdfjs-dist/build/pdf.worker.min.mjs',
     import.meta.url,
   ).toString();
-*/
 
 function Resume() {
     return (
         <Container fluid className="page-body">
             <Col>
-                {/* 
                 <Button
                     className="project-buttons"
                     variant="primary"
@@ -26,22 +23,20 @@ function Resume() {
                     target="_blank"
                     style={{ maxWidth: "200px", marginBottom:"20px" }}
                 >
-                    <IoMdDownload />
+                    <IoMdDownload size={18} />
                     &nbsp;Download Resume
                 </Button>
-                */}
-
+                
+                {/* 
                 <Container style={{height:"84vh"}}>
                     <iframe title="hi" src={pdf} width="100%" height="100%" />
                 </Container>
+                */}
 
-                {/* 
                 <Document file={pdf} className="d-flex justify-content-center">
                     <Page pageNumber={1} scale={1.5} />
                 </Document> 
-                */}
 
-                {/* 
                 <Button
                     className="project-buttons"
                     variant="primary"
@@ -49,10 +44,9 @@ function Resume() {
                     target="_blank"
                     style={{ maxWidth: "200px", marginTop:"20px" }}
                 >
-                    <IoMdDownload />
+                    <IoMdDownload size={18} />
                     &nbsp;Download Resume
                 </Button>
-                */}
             </Col>
         </Container>
     );
