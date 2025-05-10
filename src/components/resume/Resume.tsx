d61f7a89ce2bdde1789fe1b477fb7bddf7ffe7aa
jacob-myers <jakemyers190@gmail.com>
2025-05-10 18:15:12 -0400

    Using a styalized dark resume for display, with printable one still white. also updated resume.

--- src/components/resume/Resume.tsx
+++ src/components/resume/Resume.tsx
@@ -2,7 +2,8 @@ import { Container, Col, Button } from "react-bootstrap";
 
 import { IoMdDownload } from "react-icons/io";
 
-import pdf from "../../Assets/Resume_Jacob_Myers.pdf";
+import display_pdf from "../../Assets/Resume_Jacob_Myers_Display.pdf";
+import print_pdf from "../../Assets/Resume_Jacob_Myers_Print.pdf";
 
 function Resume() {
     return (
@@ -11,16 +12,27 @@ function Resume() {
                 <Button
                     className="project-buttons"
                     variant="primary"
-                    href={pdf}
+                    href={display_pdf}
                     target="_blank"
-                    style={{ maxWidth: "200px", marginBottom:"20px" }}
+                    style={{ maxWidth: "200px", marginBottom:"20px", marginRight:"20px" }}
                 >
                     <IoMdDownload size={18} />
                     &nbsp;Download Resume
                 </Button>
+
+                <Button
+                    className="project-buttons"
+                    variant="primary"
+                    href={print_pdf}
+                    target="_blank"
+                    style={{ maxWidth: "200px", marginBottom:"20px" }}
+                >
+                    <IoMdDownload size={18} />
+                    &nbsp;Download For Print
+                </Button>
                 
                 <Container style={{height:"100%"}}>
-                    <iframe title="resume" src={pdf} width="100%" height="100%" />
+                    <iframe title="resume" src={display_pdf} width="100%" height="100%" />
                 </Container>
             </Col>
         </Container>
